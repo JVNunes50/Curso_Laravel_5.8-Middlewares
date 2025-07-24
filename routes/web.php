ad62eefd4dcdfb8b3d610cde1a46493dc843a54e
JVNunes50 <jvnunes1944@gmail.com>
2025-07-23 22:22:29 -0300

    Passando parâmetros aos middlewares

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,7 @@
 use App\Http\Middleware\PrimeiroMiddleware;
 
 Route::get('/usuarios', 'UsuarioControlador@index')->middleware('primeiro', 'segundo');
+
+Route::get('/terceiro', function(){
+    return 'Passou pelo terceiro middlewere';
+})->middleware('terceiro:joao, 20');
